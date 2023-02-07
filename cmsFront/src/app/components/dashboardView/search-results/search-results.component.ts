ac9a1e02d939313e7a1106947f468d5e0fac3db1
jayber <jayber1@gmail.com>
2023-02-07 13:08:12 +0200

    responsivity fixes

--- cmsFront/src/app/components/dashboardView/search-results/search-results.component.ts
+++ cmsFront/src/app/components/dashboardView/search-results/search-results.component.ts
@@ -6,70 +6,58 @@ import { CostumerService } from 'src/app/services/costumersService/cosutmers.ser
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+  styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent  implements OnInit{
+export class SearchResultsComponent implements OnInit {
   constructor(
-    private router:ActivatedRoute,
-    private costumers:CostumerService
-  ){}
+    private router: ActivatedRoute,
+    private costumers: CostumerService
+  ) {}
 
-  data:any
-  costumersData:string[]
-  from:number = 0
-  makeCall:boolean = true
-  param:string
+  data: any;
+  costumersData: string[];
+  from: number = 0;
+  makeCall: boolean = true;
+  param: string;
 
-  perv(){
-    if(this.from > 0){
-      
-      console.log(this.from);
-      this.from = this.from - 15
-      this.serverCall(this.from)
-      
-    } 
-  }
-  next(){  
-    if(this.makeCall){
-    this.from = this.from + 15
-    this.serverCall(this.from)
+  perv() {
+    if (this.from > 0) {
+      this.from = this.from - 15;
+      this.serverCall(this.from);
     }
-    // console.log(this.from);  
+  }
+  next() {
+    if (this.makeCall) {
+      this.from = this.from + 15;
+      this.serverCall(this.from);
+    }
   }
 
-  serverCall(from){
-    console.log(from);
-    this.costumers.searchCostumer(this.param,from).subscribe(data => {
-      console.log(data);
-      this.data = data
-      console.log(this.data.length);
-      
-      if(this.data.length != 15){
-        this.makeCall = false
+  serverCall(from) {
+    this.costumers.searchCostumer(this.param, from).subscribe((data) => {
+      this.data = data;
+
+      if (this.data.length != 15) {
+        this.makeCall = false;
       } else {
-        this.makeCall = true
+        this.makeCall = true;
       }
-    })
+    });
   }
 
-  ngOnInit():void {
-    
-    this.router.params.subscribe(param => {
-      
-      this.param = param['query'];  
-      this.costumers.searchCostumer(this.param,this.from).subscribe(data=>{
-        
-        this.data = data
+  ngOnInit(): void {
+    this.router.params.subscribe((param) => {
+      this.param = param['query'];
+      this.costumers.searchCostumer(this.param, this.from).subscribe((data) => {
+        this.data = data;
 
-        if(this.data.length != 15){
-          this.makeCall = false
+        if (this.data.length != 15) {
+          this.makeCall = false;
         } else {
-          this.makeCall = true
+          this.makeCall = true;
         }
-      })
-    })
-
-    
+      });
+    });
   }
 }
 

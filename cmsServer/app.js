3969f162c4f05c57803167d798a4465a1fe5d587
Evgeny Bershdsky <79035534+jayber-dev@users.noreply.github.com>
2023-02-25 18:56:08 +0200

    ahhhhh

--- cmsServer/app.js
+++ cmsServer/app.js
@@ -23,7 +23,7 @@ app.use(session({
 }))
 
 app.use(cors({
-  origin: 'https://cmshackeru.netlify.app/',
+  origin: 'https://cmshackeru.netlify.app',
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,

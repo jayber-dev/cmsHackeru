bd8b0430edaf76f827ab71635979a4282db6f6b7
Evgeny Bershdsky <79035534+jayber-dev@users.noreply.github.com>
2023-02-28 18:39:39 +0200

    auth guard fix

--- cmsServer/routes/costumers/costumers.js
+++ cmsServer/routes/costumers/costumers.js
@@ -6,19 +6,19 @@ const costumers = express.Router()
 costumers.use((req, res, next) => {
     next()
 })
-costumers.get('/',auth.authGuard,costumersUtil.getCostumers, (req,res) => {})
+costumers.get('/', auth.authGuard, costumersUtil.getCostumers, (req, res) => { })
 
-costumers.get('/getAll',auth.authGuard,costumersUtil.getAllCostumers, (req,res) => {})
+costumers.get('/getAll', auth.authGuard, costumersUtil.getAllCostumers, (req, res) => { })
 
-costumers.get('/costumer/:id',auth.authGuard, costumersUtil.getSingleCostumer,(req,res,next) =>{})
+costumers.get('/costumer/:id', auth.authGuard, costumersUtil.getSingleCostumer, (req, res, next) => { })
 
-costumers.get('/search/:query/:searchParam',auth.authGuard,costumersUtil.findCostumer, (req,res) => {})
+costumers.get('/search/:query/:searchParam', auth.authGuard, costumersUtil.findCostumer, (req, res) => { })
 
-costumers.post('/addCostumer',auth.authGuard,costumersUtil.addCostumer,(req,res) => {})
+costumers.post('/addCostumer', auth.authGuard, costumersUtil.addCostumer, (req, res) => { })
 
-costumers.post('/editCostumer',auth.authGuard,costumersUtil.editCostumer,(req,res) => {})
+costumers.post('/editCostumer', auth.authGuard, costumersUtil.editCostumer, (req, res) => { })
 
-costumers.delete('/deleteCostumer/:id',auth.authGuard, costumersUtil.deleteCostumer, (req,res) =>{
-        
+costumers.delete('/deleteCostumer/:id', auth.authGuard, costumersUtil.deleteCostumer, (req, res) => {
+
 })
 module.exports = costumers;

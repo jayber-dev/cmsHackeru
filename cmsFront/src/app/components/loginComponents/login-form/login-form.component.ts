1bd16f8610a924954af556bef9f6789fc5751985
Evgeny Bershdsky <79035534+jayber-dev@users.noreply.github.com>
2023-03-01 22:22:03 +0200

    auth will work soon

--- cmsFront/src/app/components/loginComponents/login-form/login-form.component.ts
+++ cmsFront/src/app/components/loginComponents/login-form/login-form.component.ts
@@ -34,7 +34,7 @@ export class LoginFormComponent {
         this.router.navigateByUrl('dashboard/costumers/table/0')
         console.log(data);
         
-        this.cookieService.set('log', data['t'] )
+        this.cookieService.set('log', data['t'],{expires:1}  )
       } else {
         this.message = data['message'] 
       }

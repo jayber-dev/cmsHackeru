033ad123b7bb34a1e71962eb5e78e32076ada44e
Evgeny Bershadsky <evgenyber@eed.ef.technion.ac.il>
2023-03-05 16:13:14 +0200

    http get fix

--- cmsServer/db-utilitys/costumers.js
+++ cmsServer/db-utilitys/costumers.js
@@ -75,10 +75,10 @@ function deleteCostumer(req,res) {
     let query = `DELETE FROM costumers WHERE id=${req.params.id}`
     conn.execute(query, (err,row,fields) => {      
         if (err) console.log(err);       
-        res.sendStatus(200)   
+           
     })
     conn.end() 
-    
+    res.sendStatus(200)
 }
 
 function editCostumer(req,res){
